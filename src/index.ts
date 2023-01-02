10bd66719174a20383ac46ba3906817a75c417ee
William Zheng <williamzheng2001@gmail.com>
2023-01-02 15:24:37 -0500

    Specify fallback port

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ dotenv.config();
 
 import app from './app';
 
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
