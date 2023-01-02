ba6605f3ca30e7caac2bba67fff7d801a260ef02
William Zheng <williamzheng2001@gmail.com>
2023-01-02 14:43:45 -0500

    Use middleware to parse JSON

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => res.send('hi'));
 
 export default app;
