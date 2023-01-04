441a6bcde6904d679cd08d7d6f61752eff14ca51
William Zheng <williamzheng2001@gmail.com>
2023-01-04 08:45:48 -0500

    Create PUT request handler for updating a category

--- src/controllers/category-controller.ts
+++ src/controllers/category-controller.ts
@@ -52,4 +52,36 @@ async function getCategory(req: Request, res: Response) {
   res.json(category);
 }
 
-export { getCategories, createCategory, getCategory };
+async function updateCategory(req: Request, res: Response) {
+  // Find validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { id } = req.params;
+  const { name } = req.body;
+
+  // Find category with provided id
+  const category = await Category.findById(id);
+
+  // Throw error if category with provided id doesn't exist
+  if (!category) {
+    res.status(404);
+    throw new Error(`Could not find category with id ${id}`);
+  }
+
+  // Throw error if category with provided name already exists
+  if (await Category.findOne({ name })) {
+    res.status(400);
+    throw new Error(`A category with name '${name}' already exists`);
+  }
+
+  // Category exists and name is unused, update it
+  category.name = name;
+  await category.save();
+
+  res.json(category);
+}
+
+export { getCategories, createCategory, getCategory, updateCategory };
