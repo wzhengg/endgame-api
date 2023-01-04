c996c2b943b35f624aa8a401635e675d41f65fc0
William Zheng <williamzheng2001@gmail.com>
2023-01-03 22:17:24 -0500

    Create POST request handler for creating category

--- src/controllers/category-controller.ts
+++ src/controllers/category-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
 import Category from '../models/category-model';
 
 async function getCategories(req: Request, res: Response) {
@@ -6,4 +7,27 @@ async function getCategories(req: Request, res: Response) {
   res.json(categories);
 }
 
-export { getCategories };
+async function createCategory(req: Request, res: Response) {
+  // Find validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name } = req.body;
+
+  // Try to find a category with the provided name
+  let category = await Category.findOne({ name });
+
+  // Throw error if category with provided name already exists
+  if (category) {
+    res.status(400);
+    throw new Error(`Category with name ${name} already exists`);
+  }
+
+  // Category doesn't exist, create it
+  category = await Category.create({ name });
+  res.json(category);
+}
+
+export { getCategories, createCategory };
