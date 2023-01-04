231933e6fb0652e9fa35a6ebcc5e85b71521142c
William Zheng <williamzheng2001@gmail.com>
2023-01-04 11:28:33 -0500

    Use products router

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import connectDB from './config/db';
 import { errorHandler } from './middleware/error-middleware';
 
 import categoryRoutes from './routes/category-routes';
+import productRoutes from './routes/product-routes';
 
 connectDB();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/categories', categoryRoutes);
+app.use('/api/products', productRoutes);
 
 app.use(errorHandler);
 
