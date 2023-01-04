7f55498434daf1b72c643e09434be7f7a041b91a
William Zheng <williamzheng2001@gmail.com>
2023-01-03 22:15:25 -0500

    Create POST route for creating a category

--- src/routes/category-routes.ts
+++ src/routes/category-routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
-import { getCategories } from '../controllers/category-controller';
+import { body } from 'express-validator';
+import {
+  createCategory,
+  getCategories,
+} from '../controllers/category-controller';
 
 const router = express.Router();
 
 // GET request for all categories
 router.get('/', getCategories);
+
+// POST request for creating a category
+router.post('/', [
+  body('name', 'Category name is required').trim().notEmpty().escape(),
+  createCategory,
+]);
 
 export default router;
