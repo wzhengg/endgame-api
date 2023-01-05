794e1245475d8c349a10d644cebb5d4385f11578
William Zheng <williamzheng2001@gmail.com>
2023-01-05 10:32:18 -0500

    Create PUT route for updating products

--- src/routes/product-routes.ts
+++ src/routes/product-routes.ts
@@ -4,6 +4,7 @@ import {
   getProducts,
   createProduct,
   getProduct,
+  updateProduct,
 } from '../controllers/product-controller';
 
 const router = express.Router();
@@ -29,5 +30,22 @@ router.post(
 
 // GET request for a specific product
 router.get('/:id', param('id').exists().isMongoId(), getProduct);
+
+// PUT request for updating a product
+router.put(
+  '/:id',
+  param('id').exists().isMongoId(),
+  body('name', 'Product name is required').trim().notEmpty().escape(),
+  body('price')
+    .trim()
+    .notEmpty()
+    .withMessage('Product price is required')
+    .isCurrency()
+    .withMessage('Product price must be valid')
+    .escape(),
+  body('category').notEmpty().isMongoId(),
+  body('images').trim().notEmpty().isURL().escape(),
+  updateProduct
+);
 
 export default router;
