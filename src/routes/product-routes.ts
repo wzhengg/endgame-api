075d1d5fd070d96e7fdbfa789ede4792fad4661e
William Zheng <williamzheng2001@gmail.com>
2023-01-05 09:41:48 -0500

    Create POST route for creating a product

--- src/routes/product-routes.ts
+++ src/routes/product-routes.ts
@@ -1,9 +1,26 @@
 import express from 'express';
-import { getProducts } from '../controllers/product-controller';
+import { body } from 'express-validator';
+import { getProducts, createProduct } from '../controllers/product-controller';
 
 const router = express.Router();
 
 // GET request for all products
 router.get('/', getProducts);
+
+// POST request for creating a product
+router.post(
+  '/',
+  body('name', 'Product name is required').trim().notEmpty().escape(),
+  body('price')
+    .trim()
+    .notEmpty()
+    .withMessage('Product price is required')
+    .isCurrency()
+    .withMessage('Product price must be valid')
+    .escape(),
+  body('category').notEmpty().isMongoId(),
+  body('images').trim().notEmpty().isURL().escape(),
+  createProduct
+);
 
 export default router;
