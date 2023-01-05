6582a6fa18df272abb234538cd9d58461dcef7c2
William Zheng <williamzheng2001@gmail.com>
2023-01-05 09:42:57 -0500

    Create handler for creating products

--- src/controllers/product-controller.ts
+++ src/controllers/product-controller.ts
@@ -1,9 +1,38 @@
 import { Request, Response } from 'express';
+import { validationResult } from 'express-validator';
 import Product from '../models/product-model';
+import Category from '../models/category-model';
 
 async function getProducts(req: Request, res: Response) {
   const products = await Product.find();
   res.json(products);
 }
 
-export { getProducts };
+async function createProduct(req: Request, res: Response) {
+  // Find validation errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  // Find category with provided id
+  const category = await Category.findById(req.body.category);
+
+  // Send 404 response if category with provided id doesn't exist
+  if (!category) {
+    res.status(404);
+    throw new Error(`Could not find category with id '${req.body.category}'`);
+  }
+
+  // Create product
+  const product = await Product.create({
+    name: req.body.name,
+    price: req.body.price,
+    category: req.body.category,
+    images: [req.body.images],
+  });
+
+  res.json(product);
+}
+
+export { getProducts, createProduct };
