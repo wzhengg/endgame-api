5dc616da8d59f154e4a698d53db5c8d5f67c5ec1
William Zheng <williamzheng2001@gmail.com>
2023-01-07 17:31:01 -0500

    Create POST route for logging in users

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,10 +1,24 @@
 import express from 'express';
+import { body } from 'express-validator';
 import { validateRequest } from '../middleware/validation-middleware';
-import { bodyValidation, createUser } from '../controllers/user-controller';
+import {
+  createUser,
+  loginUser,
+  bodyValidation,
+} from '../controllers/user-controller';
 
 const router = express.Router();
 
 // POST request for creating a user
 router.post('/', bodyValidation(), validateRequest, createUser);
+
+// POST request for logging in a user
+router.post(
+  '/login',
+  body('email', 'Email is required').trim().isEmail().normalizeEmail().escape(),
+  body('password', 'Password is required').trim().escape(),
+  validateRequest,
+  loginUser
+);
 
 export default router;
